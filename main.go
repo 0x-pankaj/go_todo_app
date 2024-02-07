4fbb0bbdea7eef37a2d9cc848ccf1096e37e29ae
pankaj <0xpankaj@gmail.com>
2024-02-07 22:06:19 +0545

    routes

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Todo struct {
@@ -17,8 +20,34 @@ type User struct {
 
 var Todos []Todo
 
+//middleware
+
+func (c *Todo) isEmpty() bool {
+	return c.Todo == ""
+}
+
 func main() {
 	fmt.Println("todo app initilization")
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", homeServe).Methods("GET")
+	r.HandleFunc("/todos", getAllTodos).Methods("GET")
+	r.HandleFunc("/todo/{id}", getOneTodo).Methods("GET")
+	r.HandleFunc("/todo/{user}", getUserTodo).Methods("GET")
+	r.HandleFunc("/todo", createTodo).Methods("POST")
+	r.HandleFunc("/todo/{id}", updateTodo).Methods("PUT")
+	r.HandleFunc("/todo/{id}", deleteTodo).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":5000", r))
+}
+
+func homeServe(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Home Router init")
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write([]byte("<h1> Welcome To Todo App </h1> "))
+
 }
 
 func getAllTodos(w http.ResponseWriter, r *http.Request) {
@@ -26,6 +55,10 @@ func getAllTodos(w http.ResponseWriter, r *http.Request) {
 }
 
 func getOneTodo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getUserTodo(w http.ResponseWriter, r *http.Request) {
 
 }
 
